ef745ad7536544d390b47ebdcbe5fdf062990ff5
Cristian Medina <cemp493@gmail.com>
2026-02-25 08:53:33 -0600

    Cambios en el recuperar

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, "Public")));
 
 // Esta es tu primera "ruta"
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/Public/pages/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/Public/pages/index.html");
 });
 
-app.get('/bienvenida', (req, res) => {
-  res.sendFile(__dirname + '/Public/pages/bienvenida.html');
+app.get("/bienvenida", (req, res) => {
+  res.sendFile(__dirname + "/Public/pages/bienvenida.html");
+});
+
+app.get("/recuperar_contrasena", (req, res) => {
+  res.sendFile(__dirname + "/Public/pages/recuperar_contrasena.html");
 });
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
-
