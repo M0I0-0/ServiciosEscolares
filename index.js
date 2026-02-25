2e8166d208060b1b9935a07e4f3df3c1e508a00d
Cristian Medina <cemp493@gmail.com>
2026-02-25 09:24:10 -0600

    cambio de errores

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get("/recuperar_contrasena", (req, res) => {
   res.sendFile(__dirname + "/Public/pages/recuperar_contrasena.html");
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/Public/pages/index.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/Public/pages/index.html");
+});
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
